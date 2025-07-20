28503b0d7186fa6765598ea7afc50f9ce6e9cfc1
andrew-dragoslavic <andrewdrago39@gmail.com>
2025-07-20 19:51:54 -0400

    Front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    fetch('/api/test')
+      .then(response => response.json())
+      .then(result => setResponse(result))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {response ? <p>{response.message}</p> : <p>Loading...</p>}
     </div>
   );
 }
