48c0ddec82c192d161d879019e1e3c79e4cf238f
andrew-dragoslavic <andrewdrago39@gmail.com>
2025-07-23 14:23:40 -0400

    Key Removal

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLOnZk2n5j6hWvUp94rM6bJclHMsGXxcQ",
-  authDomain: "food-app-57687.firebaseapp.com",
-  projectId: "food-app-57687",
-  storageBucket: "food-app-57687.firebasestorage.app",
-  messagingSenderId: "961555492193",
-  appId: "1:961555492193:web:a61b1e5589d0070e892920",
-  measurementId: "G-QEDLFD90E8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
